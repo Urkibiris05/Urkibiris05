01d2e59f450a1983cff8789e8766d082c5c920c5
Urkibiris05 <ubidaurre001@ikasle.ehu.eus>
2026-01-28 15:25:01 +0100

    2 praktika

---
+++ 3 Maila/EHES/labos/2 praktika/.latexmkrc
@@ -0,0 +1,6 @@
+# Directorio para archivos auxiliares
+$aux_dir = "build";
+$out_dir = ".";
+
+# Asegurarse de que el directorio existe
+system("mkdir -p $aux_dir");
--- 3 Maila/EHES/labos/EHES/src/prak1/kodeOinarria.java
+++ 3 Maila/EHES/labos/EHES/src/prak1/kodeOinarria.java
@@ -1,4 +1,79 @@
 package prak1;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Random;
+import weka.attributeSelection.BestFirst;
+import weka.attributeSelection.CfsSubsetEval;
+import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.lazy.IB1;
+import weka.core.Capabilities;
+import weka.core.Instances;
+import weka.filters.Filter;
+import weka.filters.supervised.attribute.AttributeSelection;
+
 public class kodeOinarria {
+    public static void main(String[] args) throws Exception {
+        /////////////////////////////////////////////////////////////
+        FileReader fi = null;
+        try {
+            fi = new FileReader("~/software/weka-3-6-9/data/breast-cancer.arff");
+        } catch (FileNotFoundException e) {
+            System.out.println("ERROR: Revisar path del fichero de datos:" + args[0]);
+        }
+
+        Instances data = null;
+        try {
+            data = new Instances(fi);
+        } catch (IOException e) {
+            System.out.println("ERROR: Revisar contenido del fichero de datos: " + args[0]);
+        }
+
+        try {
+            fi.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+        }
+
+        data.setClassIndex(data.numAttributes() - 1);
+
+
+        /////////////////////////////////////////////////////////////
+        AttributeSelection filter = new AttributeSelection();
+        CfsSubsetEval eval = new CfsSubsetEval();
+        BestFirst search = new BestFirst();
+        filter.setEvaluator(eval);
+        filter.setSearch(search);
+        filter.setInputFormat(data);
+
+        Instances newData = Filter.useFilter(data, filter);
+
+
+        /////////////////////////////////////////////////////////////
+        //
+        NaiveBayes estimador = new NaiveBayes();
+
+        //
+        Evaluation evaluator = new Evaluation(newData);
+        evaluator.crossValidateModel(estimador, newData, 10, new Random(1)); // Random(1): the seed=1 means "no shuffle" :-!
+        //
+        double acc = evaluator.pctCorrect();
+        double inc = evaluator.pctIncorrect();
+        double kappa = evaluator.kappa();
+        double mae = evaluator.meanAbsoluteError();
+        double rmse = evaluator.rootMeanSquaredError();
+        double rae = evaluator.relativeAbsoluteError();
+        double rrse = evaluator.rootRelativeSquaredError();
+        double confMatrix[][] = evaluator.confusionMatrix();
+
+        System.out.println("Correctly Classified Instances  " + acc);
+        System.out.println("Incorrectly Classified Instances  " + inc);
+        System.out.println("Kappa statistic  " + kappa);
+        System.out.println("Mean absolute error  " + mae);
+        System.out.println("Root mean squared error  " + rmse);
+        System.out.println("Relative absolute error  " + rae);
+        System.out.println("Root relative squared error  " + rrse);
+    }
 }
